485a0550911fd3ed7a94d139f298a5853573b4c4
John Torakis <john.torakis+psychotropes@gmail.com>
2024-01-06 21:05:04 +0200

    feat: add APA CSS and JS

---
+++ static/js/apa.js
@@ -0,0 +1,23 @@
+/****************
+  Created by John Torakis for PsychoTropes.wiki
+
+  Referencing APA (7th Edition) Style Guides:
+  - https://apastyle.apa.org
+  - https://owl.purdue.edu/owl/research_and_citation/apa_style
+
+To be used along with /css/apa.css
+
+*/
+
+/**
+	Reference Title Italics
+*/
+reference_title_class = "reference-title"
+
+references = document.getElementById('apa-references');
+console.log(references.innerHTML);
+references.innerHTML = references.innerHTML.replace(
+	/^(.*?\(\d+\)\.)(.*?\.)(.*)$/gm,
+	`$1<span class=\"${reference_title_class}\">$2</span>$3`
+	);
+
